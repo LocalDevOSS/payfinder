e7b518f6e591b43655bec61c107728116ffaa99b
syeol.kim <syeol.kim@daum.net>
2021-08-17 02:18:33 +0900

    init commit

---
+++ src/main/java/org/localdevelopers/payfinder/PayFinderApplication.java
@@ -0,0 +1,13 @@
+package org.localdevelopers.payfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayFinderApplication.class, args);
+	}
+
+}
