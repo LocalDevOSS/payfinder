0d5f7a7a16245c5bb9b142f2ae3384277ecb5bd4
syeol.kim <syeol.kim@daum.net>
2021-09-02 22:05:37 +0900

    [PS-6] Add git secret variables

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/opt/payfinder
 cd $REPOSITORY
 
-APP_NAME=action_codedeploy
+APP_NAME=payfinder
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
