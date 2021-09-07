5be858e922694d2af7f1eeb43e5e2b386c15ff77
ywoo-park2 <ywoo.park2@navercorp.com>
2021-09-07 23:26:11 +0900

    cors mapping config 설정 추가

--- src/main/java/org/localdevelopers/payfinder/config/WebConfig.java
+++ src/main/java/org/localdevelopers/payfinder/config/WebConfig.java
@@ -2,14 +2,14 @@ package org.localdevelopers.payfinder.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost");
+                .allowedOrigins("http://localhost:3000", "http://payfinder.site");
     }
 }
